63c3acf293360281f9ba370cd89c61bdff30ea69
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-02-18 23:09:08 +0000

    return the sum of first and last digit

---
+++ src/com/gmail/ncs/sapi/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.ncs.sapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("the sum of the number's digit is: " + sumFirstAndLastDigit(12578));
+        System.out.println("the sum of the number's digit is: " + sumFirstAndLastDigit(-156));
+        System.out.println("the sum of the number's digit is: " + sumFirstAndLastDigit(8));
+        System.out.println("the sum of the number's digit is: " + sumFirstAndLastDigit(125));
+    }
+}
